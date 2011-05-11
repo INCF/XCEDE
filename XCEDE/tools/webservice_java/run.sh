1e9f09a0acac2a4136de39d0da4f15396617e628
Syam Gadde <gadde@biac.duke.edu>
2011-05-11 17:19:43 +0000

    Remove site-specific path from Makefile

--- XCEDE/tools/webservice_java/run.sh
+++ XCEDE/tools/webservice_java/run.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 # set this to the path to the "java" executable -- must be 1.5 or newer
-JAVA=/usr/java/jdk1.6.0_06/bin/java
+JAVA=${JAVA_HOME}/bin/java
 
 CLASSPATH=src:xalan-j_2_7_1/serializer.jar:xalan-j_2_7_1/xercesImpl.jar:xalan-j_2_7_1/xml-apis.jar:xalan-j_2_7_1/xalan.jar:restlet-1.0.10/lib/com.noelios.restlet.jar:restlet-1.0.10/lib/org.restlet.jar:restlet-1.0.10/lib/com.noelios.restlet.ext.simple_3.1.jar:restlet-1.0.10/lib/org.simpleframework_3.1/org.simpleframework.jar
 
